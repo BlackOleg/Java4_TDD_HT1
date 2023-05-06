d59455cd3d2b06c60892103b60c6a6406d6aa480
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-05-06 22:55:43 +0400

    findByNumber implemented

--- src/main/java/ru/olegivanov/PhoneBook.java
+++ src/main/java/ru/olegivanov/PhoneBook.java
@@ -2,9 +2,11 @@ package ru.olegivanov;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PhoneBook {
-    private static Map<String,Integer> phones = new HashMap<>();
+    private static Map<String, Integer> phones = new HashMap<>();
+
     public int add(String name, int number) {
         if (!phones.containsKey(name)) {
             phones.put(name, number);
@@ -12,7 +14,18 @@ public class PhoneBook {
         }
         return 0;
     }
-    public String findByNumber(int number){
+
+    public String findByNumber(int number) {
+        if (phones.containsValue(number)) {
+            String key = phones.entrySet().stream()
+                    .filter(entry -> entry.getValue().equals(number))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList()).toString()
+                    .replace("[", "")  //remove the right bracket
+                    .replace("]", "")  //remove the left bracket
+                    .trim();
+            return key;
+        }
         return null;
     }
 }
