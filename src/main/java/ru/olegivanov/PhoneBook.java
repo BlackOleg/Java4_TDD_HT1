3c8bc4fdb3fcb5374dd0b278e66c55e427ffdca5
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-05-06 20:22:36 +0400

    add implemented

--- src/main/java/ru/olegivanov/PhoneBook.java
+++ src/main/java/ru/olegivanov/PhoneBook.java
@@ -1,7 +1,15 @@
 package ru.olegivanov;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private static Map<String,Integer> phones = new HashMap<>();
     public int add(String name, int number) {
+        if (!phones.containsKey(name)) {
+            phones.put(name, number);
+            return phones.size();
+        }
         return 0;
     }
 }
